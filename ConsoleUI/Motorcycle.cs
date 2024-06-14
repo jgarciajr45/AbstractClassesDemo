a99b5b8f69cedfd79d938b7ab2ceb974cc2fdd86
Jesus Garcia <jgarciajr45@gmail.com>
2024-06-14 12:43:14 -0700

    cars go vroom

---
+++ ConsoleUI/Motorcycle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ConsoleUI
+{
+	public class Motorcycle : Vehicle
+	{
+        public bool HasSideCart { get; set; } = false;
+
+        public override void DriveAbstract()
+        {
+            Console.WriteLine("Driving a motorcycle");
+        }
+
+        public override void DriveVirtual()
+        {
+            Console.WriteLine("Driving a motorcycle (overridden)");
+        }
+    }
+}
+
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -32,22 +32,75 @@ namespace ConsoleUI
             */
 
             // Create a list of Vehicle called vehicles
-
+           
             /*
              * Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle (use explicit typing) but use constuctors from derived classes
              * - set them up as one of each derived class
              * Set the properties with object initializer syntax
              */
-
+            Car car1 = new Car
+            {
+                Year = "1984",
+                Make = "Ford",
+                Model = "Pinto",
+                HasTrunk = true
+            };
+            Motorcycle motorcycle1 = new Motorcycle
+            {
+                Year = "1984",
+                Make = "Honda",
+                Model = "Zip",
+                HasSideCart = true
+            };
+            Vehicle vehicle1 = new Car
+            {
+                Year = "1984",
+                Make = "Mazda",
+                Model = "ZoomZoom",
+                HasTrunk = true
+            };
+            Vehicle vehicle2 = new Motorcycle
+            {
+                Year = "1984",
+                Make = "Vespa",
+                Model = "Piaggio",
+                HasSideCart = false
+            };
             /*
              * Add the 4 vehicles to the list
              * Using a foreach loop iterate over each of the properties
              */
+            List<Vehicle> vehicles = new List<Vehicle>
+            {
+                car1,
+                motorcycle1,
+                vehicle1,
+                vehicle2
+            };
 
+            foreach (var vehicle in vehicles)
+            {
+                Console.WriteLine($"Year: {vehicle.Year}, Make: {vehicle.Make}, Model: {vehicle.Model}");
+                if (vehicle is Car car)
+                {
+                    Console.WriteLine($"It has a trunk: {car.HasTrunk}");
+                }
+                else if (vehicle is Motorcycle motorcylce)
+                {
+                    Console.WriteLine($"It has a sidecart: {motorcylce.HasSideCart}");
+                }
+                Console.WriteLine();
+            }
             // Call each of the drive methods for one car and one motorcycle
-
+            Console.WriteLine("Drive methods for one car:");
+            car1.DriveAbstract();
+            car1.DriveVirtual();
+            Console.WriteLine();
+            Console.WriteLine("Drive methods for one motorcycle:");
+            motorcycle1.DriveAbstract();
+            motorcycle1.DriveVirtual();
             #endregion            
-            Console.ReadLine();
+            
         }
     }
 }
